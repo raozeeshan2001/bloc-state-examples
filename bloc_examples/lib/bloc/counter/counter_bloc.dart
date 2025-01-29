f0270131f9ba2a6881db6b9b120a0596756bbc6f
raozeeshan2001 <raozeeshanhafiz@gmail.com>
2025-01-29 14:07:01 +0500

    implement counter example
    using bloc

---
+++ bloc_examples/lib/bloc/counter/counter_bloc.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:bloc_examples/bloc/counter/counter_event.dart';
+import 'package:bloc_examples/bloc/counter/counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(const CounterState()) {
+    on<IncrementCounter>(_increment); //event
+    on<DecrementCounter>(_decrement); //event
+  }
+  void _increment(IncrementCounter event, Emitter<CounterState> emit) {
+    emit(state.copyWith(counter: state.counter + 1));
+  }
+
+  void _decrement(DecrementCounter event, Emitter<CounterState> emit) {
+    emit(state.copyWith(counter: state.counter - 1));
+  }
+}
